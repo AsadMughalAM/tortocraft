d6dc491ee573b1625379aa3683045751a58a0f2c
AsadMughalAM <am127955@gmail.com>
2025-10-19 18:09:27 +0500

    endpoint

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Raleway, Nunito } from "next/font/google";
 import "./globals.css";
 import ClientLayout from "@/components/ClientLayout";
+import { icons } from "lucide-react";
 
 const raleway = Raleway({ subsets: ["latin"], weight: ["600", "700"] });
 const nunito = Nunito({ subsets: ["latin"], weight: ["400", "500"] });
@@ -8,6 +9,9 @@ const nunito = Nunito({ subsets: ["latin"], weight: ["400", "500"] });
 export const metadata = {
   title: "TORTOCRAFT",
   description: "A modern e-commerce website built with Next.js",
+    icons: {
+    icon: "/favicon.png", 
+  },
 };
 
 export default function RootLayout({ children }) {
